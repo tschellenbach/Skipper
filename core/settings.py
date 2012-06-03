7fac4440f076bd6d21a872755aed732e7261a8f2
Thierry Schellenbach <thierryschellenbach@gmail.com>
2012-06-03 14:56:44 +0200

    added django bootstrap for nice forms :)

--- core/settings.py
+++ core/settings.py
@@ -143,6 +143,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
     'debug_toolbar',
+    'bootstrap',
     'gargoyle',
     'nexus', #Unfortunately gargoyle pretty much requires this
     'compressor',
--- landing/views.py
+++ landing/views.py
@@ -1,7 +1,8 @@
 # Create your views here.
 from framework.view_decorators import view_defaults
-
+from landing.forms import FeedbackForm
 
 @view_defaults
 def homepage(request):
-    pass
+    feedback_form = FeedbackForm()
+    request.context['feedback_form'] = feedback_form
